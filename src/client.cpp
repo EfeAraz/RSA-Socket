19292a546c9297abd16fcb68aa8965737bc03c52
Efe Araz <efearaz124578@gmail.com>
2024-12-03 12:07:07 +0300

    added proporties for client

--- src/client.cpp
+++ src/client.cpp
@@ -1,6 +1,8 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <sys/socket.h>
+
 // TO-DO
 // -Fix decrypt-encrypt file location
 // -Send public key to server
@@ -8,8 +10,10 @@
 
 
 class message{
+    private:
+        int message_id;
     public:
-        int sender_id ;
+        int sender_id;
         std::string content;
 
 };
@@ -17,13 +21,17 @@ class message{
 class client{
     private:
         std::string private_key;
-        std::string public_key;
+        int userID;
 
     public:
+        std::string public_key;
         std::string name;
+        std::string IP_address;
+
         client(){
             public_key = readFile("keys/public.key");
             private_key = readFile("keys/private.key");
+            // IP_address = system("ipconfig > ip.txt"); // Çalışmıyor
         } 
 
         std::string readFile(std::string file_name){ 
@@ -47,18 +55,45 @@ class client{
         std::cout << this->public_key << "\n";
     }
 
-
-    message encrypt(message msg){
+1
+    message encrypt(message msg, std::string ){
         message encrypted_msg;
+
+
         return encrypted_msg;
     }
 
     message decrypt(message msg){
         message decrypted_msg;
+        // Decrypt using private key
+        // this->private_key;
 
         return decrypted_msg;
     }
+
+    void ConnectToTheServer(){
+        // Socket Connection
+        // ...
+        
+        message firstMessage;
+        /* First Message:
+            - Username
+            - userID
+            - Public Key - 
+        */
+        firstMessage.content = this->name + "\n" 
+            + std::to_string(this->userID) + "\n"
+            + this->public_key; "\n";
+
+        // Send First Message to Server 
+
+        // Keep The Connection Alive
+    }
+
+
 };
+
+
 
 
 int main(int argc,char** argv){
@@ -72,12 +107,18 @@ int main(int argc,char** argv){
     else{
         std::string tempmsg;
         for(int i = 1; i < argc; i++){
-            std::cout << "Message "<< i << ": " << argv[i] << "\n";
-            tempmsg += argv[i];
+            if((argv[i] == "-u") && (i + 1 < argc)){
+                user.name = argv[i+1];
+                i++;
+            }
+            else{
+                std::cout << "Message "<< i << ": " << argv[i] << "\n";
+                tempmsg += *argv[i] + " "  ;
+            }
         }
         msg.content = tempmsg;
     }
     std::cout << user.name << ": " << msg.content << "\n";
-
+    
     return 0;
 }
